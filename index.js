c4ebf1fbef1ae61bc01fd796daf46d91de75a601
Rushikesh Bagade <rbagade911@gmail.com>
2023-08-06 14:15:25 +0530

    updated

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ async function main() {
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
-server.listen(8080, () => {
+server.listen( process.env.PORT, () => {
   console.log("Express start");
 });
 // schema
